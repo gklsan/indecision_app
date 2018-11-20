dc363187b6120b44c413e91c2012554e92921b4b
Gokul <pgokulmca@gmail.com>
2018-11-21 00:00:23 +0530

    Exploring jsx syntax in the src/app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,54 @@ console.log('App.js is runnging!!!!!!');
 
 // This is the JSX code
 var template = React.createElement(
-  "h1",
-  null,
-  "This is jsx type of code!!!!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "This is jsx type of code!!!!"
+    ),
+    React.createElement(
+        "p",
+        null,
+        " test "
+    ),
+    React.createElement(
+        "ul",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "First"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Second"
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Gokul P"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 28"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Bangalore"
+    )
 );
 
 var appRoot = document.getElementById("app_root");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 console.log('App.js is runnging!!!!!!');
 
 // This is the JSX code
-var template = <h1>This is jsx type of code!!!!</h1>;
+var template =  (
+    <div>
+        <h1>This is jsx type of code!!!!</h1>
+        <p> test </p>
+        <ul>
+            <li>First</li>
+            <li>Second</li>
+        </ul>
+    </div>
+);
+
+
+var templateTwo =
+    <div>
+        <h1>Gokul P</h1>
+        <p>Age: 28</p>
+        <p>Location: Bangalore</p>
+    </div>
 
 var appRoot = document.getElementById("app_root");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
